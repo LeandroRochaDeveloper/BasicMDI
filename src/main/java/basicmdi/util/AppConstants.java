f0cf8a01ecb2a6cd0cf1e47bd9fab887929f64a4
LeandroRochaDeveloper <leandro.andrade.rocha@gmail.com>
2015-06-30 19:06:17 -0300

    Controller das classes que envolvem a parte de treino do sistema (Exercício, ItemTreino, Treino e AvaliaçãoFisica), só faltando a implementação das classes que necessitam do objeto Cleinte (classes que precisam do id do mesmo).

--- src/main/java/basicmdi/util/AppConstants.java
+++ src/main/java/basicmdi/util/AppConstants.java
@@ -10,7 +10,12 @@ package basicmdi.util;
  * @author Leandro
  */
 public class AppConstants {
+    // Global
     public static final String APP_NAME = "Example";
+    // Imagens
     public static final String LOGO_PATH = "images/image.png";// size: (16x16)
+    public static final String LOGO_ICON = LOGO_PATH;// size: (16x16)
+    public final static String BACKGROUND_PATH = "images/body_bg.jpg";// Background image
+    // Opcoes
     public static final String[] TRAY_OPTIONS = {"Abrir","Fechar"};
 }
